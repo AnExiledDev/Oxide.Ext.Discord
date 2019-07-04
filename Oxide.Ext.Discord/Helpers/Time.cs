bc663b9de7ff91d82b63f592982c11f117ea9551
Tricky <52532621+uTricky@users.noreply.github.com>
2019-07-04 18:32:02 +0530

    Update Time.cs

--- Oxide.Ext.Discord/Helpers/Time.cs
+++ Oxide.Ext.Discord/Helpers/Time.cs
@@ -1,9 +1,9 @@
-﻿namespace Oxide.Ext.Discord.Helpers
+namespace Oxide.Ext.Discord.Helpers
 {
     using System;
 
     public static class Time
     {
-        public static double TimeSinceEpoch() => (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
+        public static int TimeSinceEpoch() => (int) Math.Floor((DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds);
     }
 }
